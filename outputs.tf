e354574b36a0b34a363afdb108fc016c3730e20f
chavo1 <chavdar.rakov@gmail.com>
2019-06-25 19:36:11 +0300

    output

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "public_ip" {
+  value = "${aws_instance.ec2.*.public_ip}"
+}
+output "public_dns" {
+  value = "${aws_instance.ec2.*.public_dns}"
+}
+
+output "hostname" {
+  value = "${cloudflare_record.www.*.hostname}"
+}
